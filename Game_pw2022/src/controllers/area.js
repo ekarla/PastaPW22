e97e2802319fc9712bbf07753f29cc0c77912f2c
eKarla <evs@icomp.ufam.edu.br>
2022-04-10 11:36:10 -0400

    atualzação dos arquivos

---
+++ Game_pw2022/src/controllers/area.js
@@ -0,0 +1,12 @@
+import { Area } from "../models/index";
+//const Op = models.Sequelize.Op;
+
+const index = async(req,res)=>{
+    const areas = await Area.findAll({where: true});
+    console.log(areas);
+    res.render("area/index",{ 
+        areas: areas.map((area)=> area.toJSON())
+     });
+}
+
+export default{index}
--- Game_pw2022/src/router/router.js
+++ Game_pw2022/src/router/router.js
@@ -1,9 +1,15 @@
 import express from "express";
 import mainController from "../controllers/main";
+import areaController from "../controllers/area";
 const router = express.Router();
 
+//Main  controller
 router.get("/", mainController.index);
 router.get("/about", mainController.about);
 router.get("/ui", mainController.ui);
+
+//Area Controler
+
+router.get("/areas", areaController.index);
 
 export default router;
