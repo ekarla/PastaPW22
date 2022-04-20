2fe7c54dd1510d4493b12d75def8f6f01cc5f9bb
eKarla <evs@icomp.ufam.edu.br>
2022-04-20 17:53:06 -0400

    questao04 adicionada com atraso

---
+++ Prova01/questao04.js
@@ -0,0 +1,20 @@
+const fs = require('fs');
+var p1 = new Promise(function (resolve, reject) {
+    fs.readFile('./1.txt', function(error, data) {
+    resolve(parseInt(data));
+    });
+})
+var p2 = new Promise(function (resolve, reject) {
+    fs.readFile('./2.txt', function(error, data) {
+    resolve(parseInt(data));
+    });
+})
+
+var p3 = new Promise(function (resolve, reject) {
+    fs.readFile('./3.txt', function(error, data) {
+    resolve(parseInt(data));
+    });
+})
+Promise.all([p1, p2, p3]).then(function([data1, data2, data3]) {
+    console.log(data1 + data2 + data3);
+});
